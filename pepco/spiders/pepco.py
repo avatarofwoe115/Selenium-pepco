a914fb9874ee1c4ff41a26b42a684aefbc57bc0b
realchief <stryokka@gmail.com>
2019-05-07 23:23:12 -0700

    updated result

--- pepco/spiders/pepco.py
+++ pepco/spiders/pepco.py
@@ -30,7 +30,7 @@ class PepcoSpider(Spider):
                     self.username_list.append(row[1])
                     self.password_list.append(row[2])
 
-        with open('TEST_EquityMgmt2LLC-account_number REV.csv', 'rb') as csvfile_numbers:
+        with open('EquityMgmt2LLC-account_number REV.csv', 'rb') as csvfile_numbers:
             reader_numbers = csv.reader(csvfile_numbers)
             self.accountOwnerID_list = []
             self.accountNumber_list = []
@@ -38,10 +38,15 @@ class PepcoSpider(Spider):
             self.billCycleDays_list = []
             for row_index, row in enumerate(reader_numbers):
                 if row_index != 0:
-                    self.accountOwnerID_list.append(row[0])
-                    self.accountNumber_list.append(row[1])
-                    self.lastDownloadBillDate_list.append(row[2])
-                    self.billCycleDays_list.append(row[3])
+                    last_downloaded_date = datetime.strptime(row[2], "%m/%d/%Y")
+                    cycle_date = int(row[3])
+                    if last_downloaded_date + timedelta(days=cycle_date) < datetime.now():
+                        self.accountOwnerID_list.append(row[0])
+                        self.accountNumber_list.append(row[1])
+                        self.lastDownloadBillDate_list.append(row[2])
+                        self.billCycleDays_list.append(row[3])
+                    else:
+                        pass
 
         self.user_index = 0
 
@@ -111,110 +116,100 @@ class PepcoSpider(Spider):
                     '//div[contains(@id, "changeAccountDT1_filter")]//input[contains(@type, "search")]')
 
                 account_number = self.accountNumber_list[account_index]
-                # account_number = self.driver.find_elements_by_xpath(
-                #     '//div[@class="exc-account-summary"]/p[contains(text(), "Account #")]/span')[0].text
                 # account_number = '55019181241'
 
-                last_downloaded_date = datetime.strptime(self.lastDownloadBillDate_list[account_index], "%m/%d/%Y")
-                cycle_date = int(self.billCycleDays_list[account_index])
-                if last_downloaded_date + timedelta(days=cycle_date) < datetime.now():
-                    account_number_search_input.send_keys(account_number)
-                    time.sleep(2)
-                    account_number_search_input.send_keys(Keys.ENTER)
-                    account_rows = self.driver.find_elements_by_xpath('//table[@id="changeAccountDT1"]//tbody//tr')
+                account_number_search_input.send_keys(account_number)
+                time.sleep(2)
+                account_number_search_input.send_keys(Keys.ENTER)
+                account_rows = self.driver.find_elements_by_xpath('//table[@id="changeAccountDT1"]//tbody//tr')
 
-                    if account_rows:
-                        time.sleep(5)
-                        view_button = account_rows[0].find_elements_by_xpath(
-                            './/td[@class="action-cell ng-scope"]//button')
-                        if view_button:
-                            view_button[1].click()
-                        else:
-                            pass
+                if account_rows:
+                    time.sleep(10)
+                    view_button = account_rows[0].find_elements_by_xpath(
+                        './/td[@class="action-cell ng-scope"]//button')
+                    if view_button:
+                        view_button[1].click()
                     else:
                         pass
-
-                    time.sleep(3)
-                    if self.driver.current_url != 'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx':
-                        self.driver.get(
-                            'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx')
-                        time.sleep(3)
-
-                    options = self.driver.find_elements_by_xpath('//select[@id="StatementType"]//option')
-                    if options:
-                        statement_type = options[2]
-                        statement_type.click()
-
-                    search_button = self.driver.find_elements_by_xpath(
-                        '//button[@class="btn btn-primary" and @processing-button="Processing..."]'
-                    )
-                    if search_button:
-                        search_button[0].click()
-                    else:
-                        print "There is no search button"
-
-                    time.sleep(5)
-
-                    all_pages_crawled = False
-                    while not all_pages_crawled:
-                        rows = self.driver.find_elements_by_xpath('//table//tbody//tr')
-                        if rows:
-                            row = rows[0]
-                            bill_date_info = row.find_elements_by_xpath('.//td')[0].text.split('/')
-                            if bill_date_info[0] != 'No results found.':
-                                bill_date = bill_date_info[2] + bill_date_info[0] + bill_date_info[1]
-
-                                print_btn = row.find_elements_by_xpath(
-                                    './/td//button[contains(text(), "View")]')[0]
-
-                                print '--------------- downloading -----------------'
-                                yield self.download_page(print_btn, account_number, bill_date)
-                                time.sleep(2)
-
-                                with open('TEST_EquityMgmt2LLC-account_number REV.csv', 'rb') as csv_read:
-                                    reader = csv.reader(csv_read)
-                                    lines = list(reader)
-                                    lines[account_index + 1][2] = datetime.today().strftime('%m/%d/%Y')
-
-                                with open('updated_EquityMgmt2LLC-account_number REV.csv', 'w') as csv_write:
-                                    writer = csv.writer(csv_write)
-                                    writer.writerows(lines)
-
-                                input = open('updated_EquityMgmt2LLC-account_number REV.csv', 'rb')
-                                output = open('output.csv', 'wb')
-                                writer = csv.writer(output)
-                                for row in csv.reader(input):
-                                    if row:
-                                        writer.writerow(row)
-                                input.close()
-                                output.close()
-
-                                os.remove('updated_EquityMgmt2LLC-account_number REV.csv')
-                                os.remove('TEST_EquityMgmt2LLC-account_number REV.csv')
-                                os.rename('output.csv', 'TEST_EquityMgmt2LLC-account_number REV.csv')
-
-                                time.sleep(2)
-
-                                try:
-                                    self.driver.find_elements_by_xpath('//li[@class="paginate_button next"]')[
-                                        0].click()
-                                except:
-                                    all_pages_crawled = True
-                            else:
-                                all_pages_crawled = True
-
-                    change_account_btn = self.driver.find_elements_by_xpath(
-                        '//button[@class="btn btn-primary" and contains(text(), "Change Account")]')
-                    if change_account_btn:
-                        change_account_btn[0].click()
-                    else:
-                        self.driver.get('https://secure.pepco.com/Pages/ChangeAccount.aspx')
-                    time.sleep(3)
-
                 else:
-                    print "This file has been downloaded on {}".format(self.lastDownloadBillDate_list[account_index])
-                    print "It has been {} days from when lastest downloading finished."\
-                        .format(self.billCycleDays_list[account_index])
+                    pass
+
+                time.sleep(3)
+                if self.driver.current_url != 'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx':
+                    self.driver.get(
+                        'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx')
+                    time.sleep(3)
+
+                options = self.driver.find_elements_by_xpath('//select[@id="StatementType"]//option')
+                if options:
+                    statement_type = options[2]
+                    statement_type.click()
+
+                search_button = self.driver.find_elements_by_xpath(
+                    '//button[@class="btn btn-primary" and @processing-button="Processing..."]'
+                )
+                if search_button:
+                    search_button[0].click()
+                else:
+                    print "There is no search button"
+
+                time.sleep(5)
+
+                all_pages_crawled = False
+                while not all_pages_crawled:
+                    rows = self.driver.find_elements_by_xpath('//table//tbody//tr')
+                    if rows:
+                        row = rows[0]
+                        bill_date_info = row.find_elements_by_xpath('.//td')[0].text.split('/')
+                        if bill_date_info[0] != 'No results found.':
+                            bill_date = bill_date_info[2] + bill_date_info[0] + bill_date_info[1]
+
+                            print_btn = row.find_elements_by_xpath(
+                                './/td//button[contains(text(), "View")]')[0]
+
+                            print '--------------- downloading -----------------'
+                            yield self.download_page(print_btn, account_number, bill_date)
+                            time.sleep(2)
+
+                            with open('EquityMgmt2LLC-account_number REV.csv', 'rb') as csv_read:
+                                reader = csv.reader(csv_read)
+                                lines = list(reader)
+                                lines[account_index + 1][2] = datetime.today().strftime('%m/%d/%Y')
+
+                            with open('updated_EquityMgmt2LLC-account_number REV.csv', 'w') as csv_write:
+                                writer = csv.writer(csv_write)
+                                writer.writerows(lines)
+
+                            input = open('updated_EquityMgmt2LLC-account_number REV.csv', 'rb')
+                            output = open('output.csv', 'wb')
+                            writer = csv.writer(output)
+                            for row in csv.reader(input):
+                                if row:
+                                    writer.writerow(row)
+                            input.close()
+                            output.close()
+
+                            os.remove('updated_EquityMgmt2LLC-account_number REV.csv')
+                            os.remove('EquityMgmt2LLC-account_number REV.csv')
+                            os.rename('output.csv', 'EquityMgmt2LLC-account_number REV.csv')
+
+                            time.sleep(2)
+
+                            try:
+                                self.driver.find_elements_by_xpath('//li[@class="paginate_button next"]')[
+                                    0].click()
+                            except:
+                                all_pages_crawled = True
+                        else:
+                            all_pages_crawled = True
+
+                change_account_btn = self.driver.find_elements_by_xpath(
+                    '//button[@class="btn btn-primary" and contains(text(), "Change Account")]')
+                if change_account_btn:
+                    change_account_btn[0].click()
+                else:
+                    self.driver.get('https://secure.pepco.com/Pages/ChangeAccount.aspx')
+                time.sleep(3)
 
                 account_index = account_index + 1
                 if account_index > len(self.accountOwnerID_list) - 1:
