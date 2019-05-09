caa848f1ea88b1a4b69946dd2fdcda5613b03b71
realchief <stryokka@gmail.com>
2019-05-08 22:03:11 -0700

    added sign out

--- pepco/spiders/pepco.py
+++ pepco/spiders/pepco.py
@@ -106,122 +106,129 @@ class PepcoSpider(Spider):
                         else:
                             pass
 
-            account_index = 0
-            all_numbers_option = True
-            while all_numbers_option:
+            if accountNumber_list:
+                account_index = 0
+                all_numbers_option = True
+                while all_numbers_option:
 
-                if self.driver.current_url != 'https://secure.pepco.com/Pages/ChangeAccount.aspx':
-                    self.driver.get('https://secure.pepco.com/Pages/ChangeAccount.aspx')
+                    if self.driver.current_url != 'https://secure.pepco.com/Pages/ChangeAccount.aspx':
+                        self.driver.get('https://secure.pepco.com/Pages/ChangeAccount.aspx')
 
-                time.sleep(15)
+                    time.sleep(15)
 
-                account_number_search_input = self.driver.find_element_by_xpath(
-                    '//div[contains(@id, "changeAccountDT1_filter")]//input[contains(@type, "search")]')
+                    account_number_search_input = self.driver.find_element_by_xpath(
+                        '//div[contains(@id, "changeAccountDT1_filter")]//input[contains(@type, "search")]')
 
-                account_number = accountNumber_list[account_index]
-                # account_number = '55019181241'
+                    account_number = accountNumber_list[account_index]
+                    # account_number = '55019181241'
 
-                account_number_search_input.send_keys(account_number)
-                time.sleep(2)
-                account_number_search_input.send_keys(Keys.ENTER)
-                account_rows = self.driver.find_elements_by_xpath('//table[@id="changeAccountDT1"]//tbody//tr')
+                    account_number_search_input.send_keys(account_number)
+                    time.sleep(2)
+                    account_number_search_input.send_keys(Keys.ENTER)
+                    account_rows = self.driver.find_elements_by_xpath('//table[@id="changeAccountDT1"]//tbody//tr')
 
-                if account_rows:
-                    time.sleep(10)
-                    view_button = account_rows[0].find_elements_by_xpath(
-                        './/td[@class="action-cell ng-scope"]//button')
-                    if view_button:
-                        view_button[1].click()
+                    if account_rows:
+                        time.sleep(10)
+                        view_button = account_rows[0].find_elements_by_xpath(
+                            './/td[@class="action-cell ng-scope"]//button')
+                        if view_button:
+                            view_button[1].click()
+                        else:
+                            pass
                     else:
                         pass
-                else:
-                    pass
 
-                time.sleep(3)
-                if self.driver.current_url != 'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx':
-                    self.driver.get(
-                        'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx')
+                    time.sleep(3)
+                    if self.driver.current_url != 'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx':
+                        self.driver.get(
+                            'https://secure.pepco.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx')
+                        time.sleep(3)
+
+                    options = self.driver.find_elements_by_xpath('//select[@id="StatementType"]//option')
+                    if options:
+                        statement_type = options[2]
+                        statement_type.click()
+
+                    search_button = self.driver.find_elements_by_xpath(
+                        '//button[@class="btn btn-primary" and @processing-button="Processing..."]'
+                    )
+                    if search_button:
+                        search_button[0].click()
+                    else:
+                        print "There is no search button"
+
+                    time.sleep(5)
+
+                    all_pages_crawled = False
+                    while not all_pages_crawled:
+                        rows = self.driver.find_elements_by_xpath('//table//tbody//tr')
+                        if rows:
+                            row = rows[0]
+                            bill_date_info = row.find_elements_by_xpath('.//td')[0].text.split('/')
+                            if bill_date_info[0] != 'No results found.':
+                                bill_date = bill_date_info[2] + bill_date_info[0] + bill_date_info[1]
+
+                                print_btn = row.find_elements_by_xpath(
+                                    './/td//button[contains(text(), "View")]')[0]
+
+                                print '--------------- downloading -----------------'
+                                yield self.download_page(print_btn, accountOwnerID, account_number, bill_date)
+                                time.sleep(2)
+
+                                with open(account_file_name, 'rb') as csv_read:
+                                    reader = csv.reader(csv_read)
+                                    lines = list(reader)
+                                    lines[account_index + 1][2] = datetime.today().strftime('%m/%d/%Y')
+
+                                with open(updated_file_name, 'w') as csv_write:
+                                    writer = csv.writer(csv_write)
+                                    writer.writerows(lines)
+
+                                input = open(updated_file_name, 'rb')
+                                output = open('output.csv', 'wb')
+                                writer = csv.writer(output)
+                                for row in csv.reader(input):
+                                    if row:
+                                        writer.writerow(row)
+                                input.close()
+                                output.close()
+
+                                os.remove(updated_file_name)
+                                os.remove(account_file_name)
+                                os.rename('output.csv', account_file_name)
+
+                                time.sleep(2)
+
+                                try:
+                                    self.driver.find_elements_by_xpath('//li[@class="paginate_button next"]')[
+                                        0].click()
+                                except:
+                                    all_pages_crawled = True
+                            else:
+                                all_pages_crawled = True
+
+                    change_account_btn = self.driver.find_elements_by_xpath(
+                        '//button[@class="btn btn-primary" and contains(text(), "Change Account")]')
+                    if change_account_btn:
+                        change_account_btn[0].click()
+                    else:
+                        self.driver.get('https://secure.pepco.com/Pages/ChangeAccount.aspx')
                     time.sleep(3)
 
-                options = self.driver.find_elements_by_xpath('//select[@id="StatementType"]//option')
-                if options:
-                    statement_type = options[2]
-                    statement_type.click()
+                    account_index = account_index + 1
+                    if account_index > len(accountOwnerID_list) - 1:
+                        all_numbers_option = False
+            else:
+                print('===========All files of your account have been downloaded================')
 
-                search_button = self.driver.find_elements_by_xpath(
-                    '//button[@class="btn btn-primary" and @processing-button="Processing..."]'
-                )
-                if search_button:
-                    search_button[0].click()
-                else:
-                    print "There is no search button"
+                signout_button = self.driver.find_elements_by_xpath(
+                        '//button[@class="btn btn-accent exc-sign-in-btn" and contains(text(), "Sign Out")]')
+                if signout_button:
+                        signout_button[0].click()
 
-                time.sleep(5)
-
-                all_pages_crawled = False
-                while not all_pages_crawled:
-                    rows = self.driver.find_elements_by_xpath('//table//tbody//tr')
-                    if rows:
-                        row = rows[0]
-                        bill_date_info = row.find_elements_by_xpath('.//td')[0].text.split('/')
-                        if bill_date_info[0] != 'No results found.':
-                            bill_date = bill_date_info[2] + bill_date_info[0] + bill_date_info[1]
-
-                            print_btn = row.find_elements_by_xpath(
-                                './/td//button[contains(text(), "View")]')[0]
-
-                            print '--------------- downloading -----------------'
-                            yield self.download_page(print_btn, accountOwnerID, account_number, bill_date)
-                            time.sleep(2)
-
-                            with open(account_file_name, 'rb') as csv_read:
-                                reader = csv.reader(csv_read)
-                                lines = list(reader)
-                                lines[account_index + 1][2] = datetime.today().strftime('%m/%d/%Y')
-
-                            with open(updated_file_name, 'w') as csv_write:
-                                writer = csv.writer(csv_write)
-                                writer.writerows(lines)
-
-                            input = open(updated_file_name, 'rb')
-                            output = open('output.csv', 'wb')
-                            writer = csv.writer(output)
-                            for row in csv.reader(input):
-                                if row:
-                                    writer.writerow(row)
-                            input.close()
-                            output.close()
-
-                            os.remove(updated_file_name)
-                            os.remove(account_file_name)
-                            os.rename('output.csv', account_file_name)
-
-                            time.sleep(2)
-
-                            try:
-                                self.driver.find_elements_by_xpath('//li[@class="paginate_button next"]')[
-                                    0].click()
-                            except:
-                                all_pages_crawled = True
-                        else:
-                            all_pages_crawled = True
-
-                change_account_btn = self.driver.find_elements_by_xpath(
-                    '//button[@class="btn btn-primary" and contains(text(), "Change Account")]')
-                if change_account_btn:
-                    change_account_btn[0].click()
-                else:
-                    self.driver.get('https://secure.pepco.com/Pages/ChangeAccount.aspx')
-                time.sleep(3)
-
-                account_index = account_index + 1
-                if account_index > len(accountOwnerID_list) - 1:
-                    all_numbers_option = False
-
-            print('===========All files of your account have been downloaded================')
-            user_index = user_index + 1
-            if user_index > len(self.accountOwnerID_credential_list) - 1:
-                all_users_option = False
+                user_index = user_index + 1
+                if user_index > len(self.accountOwnerID_credential_list) - 1:
+                    all_users_option = False
 
         print('===========All files of all users have been downloaded================')
         self.driver.close()
